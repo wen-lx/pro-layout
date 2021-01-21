c902989dbfa0c3362290a331ea5c29d0f7bc88dc
温丽香 <lixiang.wen@seetatech.com>
2021-01-21 18:07:03 +0800

    路由守卫修改

--- examples/src/permission.js
+++ examples/src/permission.js
@@ -1,18 +1,19 @@
 import router, { resetRouter } from './router'
 import store from './store'
 import { asyncRoutes } from './router/router.config'
+let flag = false
 
 router.beforeEach((to, from, next) => {
-  if (!router.resolve({name: 'exception'}).route.matched.length) {
-    try {
-      // resetRouter()
-      router.addRoutes(asyncRoutes)
-      next()
-    } catch {
-      console.log('error')
-    }
+  if (to.path === '/') {
+    next({path: '/login'});
   } else {
-    next()
+    if (flag) {
+      next()
+    } else {
+      flag = true
+      router.addRoutes(asyncRoutes) // 动态添加可访问路由表
+      next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+    }
   }
 })
 
